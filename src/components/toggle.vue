c6a127f043a759a0f23c84be614e4038833d2d9c
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-02 16:16:31 +0900

    mixin 컴포넌트 생성

---
+++ src/components/toggle.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+
+@Component
+export default class Toggle extends Vue {
+  public show: boolean = false;
+  public toggle() {
+    this.show = !this.show;
+  }
+}
+
+</script>
